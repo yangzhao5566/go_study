4d28e7fec3c253094b6084a6e910dbd0835e3f56
yangzhao5566 <yangzhao6_6@163.com>
2018-06-15 14:50:37 +0800

    update

---
+++ 流畅python/coroaverager.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+from functools import wraps
+
+
+def coroutine(func):
+    @wraps(func)
+    def primer(*args, **kwargs):
+        gen = func(*args, **kwargs)
+        next(gen)
+        return gen
+    return primer
+
+
+@coroutine
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    while True:
+        term = yield average
+        total += term
+        count += 1
+        average = total/count
